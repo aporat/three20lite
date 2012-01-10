4c2ed74eb0823c064b54990786734b98df2f93a4
Adar Porat <adar.porat@gmail.com>
2012-01-10 18:49:21 -0500

    removed TTExtension

--- src/Three20Core/Headers/Three20Core.h
+++ src/Three20Core/Headers/Three20Core.h
@@ -30,11 +30,6 @@
 // - Debug
 #import "Three20Core/TTDebug.h"
 
-// - Extensions
-#import "Three20Core/TTExtensionInfo.h"
-#import "Three20Core/TTExtensionAuthor.h"
-#import "Three20Core/TTExtensionLoader.h"
-
 // - Entity Tables
 #import "Three20Core/TTEntityTables.h"
 
--- src/Three20UI/Headers/Three20UI.h
+++ src/Three20UI/Headers/Three20UI.h
@@ -19,7 +19,6 @@
 #import "Three20UI/TTViewController.h"
 #import "Three20UI/TTSplitViewController.h"
 #import "Three20UI/TTNavigationController.h"
-#import "Three20UI/TTExtensionsController.h"
 #import "Three20UI/TTWebController.h"
 #import "Three20UI/TTMessageController.h"
 #import "Three20UI/TTMessageControllerDelegate.h"
