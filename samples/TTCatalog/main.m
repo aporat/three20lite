84b177bb07943fbd4f3379bfb5301a9a0dd08c87
Adar Porat <adar.porat@gmail.com>
2012-02-03 12:06:57 -0500

    TTCatalog

--- samples/TTCatalog/main.m
+++
@@ -1,9 +0,0 @@
-#import <Three20Lite/Three20Lite.h>
-
-int main(int argc, char *argv[]) {
-	
-	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-	int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
-	[pool release];
-	return retVal;
-}
